fd41066ebf3ab02bc5c6a815c4d22e7c710983ef
Arnestas <a.baltrusaitis@gmail.com>
2022-03-09 13:37:10 +0200

    - Test directory and file were created

---
+++ src/test/java/com/example/fg/DataBaseTest.java
@@ -0,0 +1,16 @@
+package com.example.fg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataBaseTest {
+
+    @Test
+    void insertIntoDB() {
+    }
+
+    @Test
+    void getNames() {
+    }
+}
