026cb41a0eaa97a3086a51525254bcc075bf4cb3
Arnestas <a.baltrusaitis@gmail.com>
2022-03-09 11:52:21 +0200

    - Fixed Path to DB

--- src/main/java/com/example/fg/DataBase.java
+++ src/main/java/com/example/fg/DataBase.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBase {
-    static String pathToDB = "C:\\Users\\ba_in\\IdeaProjects\\FoxGeese\\src\\main\\resources\\database\\foxandgeesedb.db";
+    static String pathToDB = "src\\main\\resources\\database\\foxandgeesedb.db";
 
     public void insertIntoDB(String name, int points, String role) {
 
