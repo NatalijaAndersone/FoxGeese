fabd8b86d99fa8b638e40144c90de58c4a31ba28
Arnestas <a.baltrusaitis@gmail.com>
2022-03-09 13:52:25 +0200

    - Filled DataBaseTest

--- src/test/java/com/example/fg/DataBaseTest.java
+++ src/test/java/com/example/fg/DataBaseTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataBaseTest {
+    DataBase db = new DataBase();
 
     @Test
     void insertIntoDB() {
+        db.insertIntoDB("TESTUKAS", 1, "T");
     }
 
     @Test
     void getNames() {
+        System.out.println(db.getNames("T"));
+       // assertEquals("TESTUKAS",db.getNames("T"));
     }
 }
